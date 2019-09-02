95551c03b699ef358d306cf3bb33b57eae7e8693
Alex Kharkhardin <xaris441@mai.ru>
2019-09-02 21:20:37 +1000

    Get Light app on device

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -5,30 +5,40 @@
 //  Created by Alex Harhardin on 01/09/2019.
 //  Copyright © 2019 Alex Harhardin. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 var lightOn = false
     @IBAction func buttonPressed(_ sender: UIButton) {
             lightOn = !lightOn
-      updateUI()
+        updateView()
     }
-    func updateUI() {
-        view.backgroundColor = lightOn ? .white : .black
-//        if lightOn {
-//            view.backgroundColor = .white
-//        } else {
-//            view.backgroundColor = .black
-//        }
+    func updateView() {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let dev = device, dev.hasTorch {
+            view.backgroundColor = .black
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = lightOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        } else {
+            view.backgroundColor = lightOn ? .white : .black
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    updateUI()
+    updateView()
     }
 
-
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
 }
 
